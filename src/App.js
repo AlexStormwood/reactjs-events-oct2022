d647fec8edf4172e19ce4ef3ef08a742ae78af7e
Alex H <alex.holder@bigfootds.com>
2023-05-02 20:51:23 +1000

    More events

--- src/App.js
+++ src/App.js
@@ -36,9 +36,65 @@ function App() {
 		event.target.style.backgroundColor = null;
 	}
 
+
+	// onkeydown 
+	function logKeyPress(event){
+		switch (event.key) {
+			case "ArrowLeft":
+			case "ArrowRight":
+			case "ArrowUp":
+			case "ArrowDown":
+				console.log("User pressed: " + event.key)
+				break;
+			default:
+				break;
+		}
+	}
+
+
+	// onblur & onfocus 
+	function logOnBlur(event){
+		console.log("Element lost focus!");
+	}
+
+	function logOnFocus(event){
+		console.log(`Focus gained! Target was ${event.target.tagName}`);
+	}
+
+
+	// onoffline and ononline
+	function logGoneOffline(event){
+		// Show a banner telling the user that no new content will load 
+		// Queueing API requests to wait until we are online again
+		// 
+	}
+
+	function logGoneOnline(event){
+		// Make an API request to say that User XYZ is now online 
+		// Remove any "You are offline" banners 
+		// Work through any queued API requests from onoffline 
+	}
+
+
+
+	// oncontextmenu (it's in Ed)
+	// Not shown because it's in Ed
+
+	// ondrag, ondragstart, ondragover, ondrop 
+	// Will maybe make it as a DIY challenge, optional challenge
+
 	return (
 		<div className="App" 
+
 		onClick={logWhenClicked} 
+
+		tabIndex={0}
+		onKeyDown={logKeyPress}
+
+		onBlur={logOnBlur}
+		onFocus={logOnFocus}
+		
+
 		style={{backgroundColor: "lightgrey", width: "100vw", height: "100vh"}}>
 
 			<h1 onMouseOver={(event) => {mousedOverHeading(event)}} onMouseOut={(event) => {mousedOutOfHeading(event)}}  >Cool Website Name</h1>
